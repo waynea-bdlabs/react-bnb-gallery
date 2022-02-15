2eb4674ae6eac4ec8b7247b8dadc15beaa8e38fb
waynea-bdlabs <wayne.abarquez@bdlabs.ventures>
2022-02-15 21:54:06 +0800

    included build files (dist)

--- docs/next.config.js
+++
@@ -1,9 +0,0 @@
-const withMdxEnhanced = require('next-mdx-enhanced');
-const withMDX = require('@next/mdx');
-
-module.exports = withMdxEnhanced({
-  layoutPath: 'layouts',
-  extendFrontMatter: {
-    phase: 'prebuild|loader|both',
-  },
-})(withMDX());
